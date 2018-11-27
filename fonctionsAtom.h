b801fdcd283b9f0fdfa5b41958636fb26573bdb1
AmySian <44503383+AmySian@users.noreply.github.com>
2018-11-28 00:25:39 +0100

    Update fonctionsAtom.h

--- fonctionsAtom.h
+++ fonctionsAtom.h
@@ -1,78 +1,40 @@
 typedef struct {
-
-	int Direction;
-
+	//char Direction;
 	int PosX;
-
 	int PosY;
-
 	//int Blindage;
-
 	//int Blindage_orig;
-
 	//int Touches;
 	int TailleX;
 	int TailleY;
-	int Carrosserie;
-
 	//char Type;
-
 	//int Etat;
-
 	//int Mise_a_jour;
-
 	//struct tank * NXT;
-
 }tank;
-
-typedef struct {
-	int Actif;
-
-	int Direction;
-
-	int PosX;
-
-	int PosY;
-
-}obus;
-
-
-
 
 char ** ALLOCATION_MAT_DYN (int NB_L, int NB_C);
 
-
-
 char ** CHARGEMENT_MAP (char * nom_fic, int NB_L, int NB_C);
-
-
 
 void CLEAR_MYTANK (int NB_L, int NB_C,int PosX, int PosY);
 
 void AFFICHE_MYTANK (char * nom_fic, int NB_L, int NB_C,int PosX, int PosY);
 
-
-
 void AFFICHAGE_MAT(int NB_L, int NB_C, char ** MAT);
 
 void AFFICHAGE_MENU (int NB_L, int NB_C, char * nom_fic);
-
 
 char key_pressed();
 
 int MENU(int NB_L, int NB_C, char * facile, char * difficile, char * quitter);
 
-void MODE_FACILE (char * nameMapFile, int mapX, int mapY, char * nameTankFile,int PX,int PY, int TX, int TY, int DIR);
+void MODE_FACILE (char * nameMapFile, int mapX, int mapY, char * nameTankFileA, char * nameTankFileB, char * nameTankFileC, int PX,int PY,int TX,int TY);
+/*
+void INCLUDE_TANK (char ** map, int NB_L, int NB_C, tank theTank);
 
-
-void INITIALIZE_TANK(char ** map,tank TANK,int PX,int PY, int TX, int TY, int DIR, char * nameFile);
-
-void INITIALIZE_OBUS(obus OBUS, int Actif, int PX,int PY, int DIR);
-
-obus * INITIALIZE_LIST_OBUS ();
-
-void DEPLACE_OBUS(obus OBUS, char ** map);
-
-//void THROW_BULLET(int TailleY, int TailleX,int PosX, int PosY, int Dir, char ** map);
+void CLEAR_TANK (char ** map, int NB_L, int NB_C, tank TANK);
+*/
+void INITIALIZE_TANK(char ** map,tank TANK,int PX,int PY, int TX, int TY, char * nameFileA);
 
 void MOVE_TANK(tank TANK,int DIR, char ** map, int NB_L, int NB_C);

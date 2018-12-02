2e06f880942e24d7f3342cdc1ad410ad2088f56b
EmmaDuSudOuest <44503569+EmmaDuSudOuest@users.noreply.github.com>
2018-12-02 22:19:40 +0100

    Update mainAtom.c

--- mainAtom.c
+++ mainAtom.c
@@ -2,19 +2,18 @@
 #include "fonctionsAtom.h"
 
 
-
 int main (int argc, char * argv[]) {
 	int choix;
 	choix = MENU(107,672,"menuFacile.txt","menuDifficile.txt","menuQuitter.txt");
 	switch (choix) {
-		//mode facile
-		case 0 :
-			MODE_FACILE("map.txt", 672, 107,"tankGauche.txt","tankDroite.txt","tankHaut",20,50,27,9,8);
-			break;
 		//quitter
 		case 2 :
 			AFFICHAGE_MENU(214, 672, "out.txt");
 			return 0;
+		//mode facile
+		default :
+			MODE_FACILE("map.txt", 672, 107,"tankGauche.txt","tankDroite.txt","tankHaut",20,50,27,9,8);
+			break;
 	}
 	return 0;
 }

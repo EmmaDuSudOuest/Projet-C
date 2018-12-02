952c17f78ce26712f6bd5c6eeee38c55ba3382e1
EmmaDuSudOuest <44503569+EmmaDuSudOuest@users.noreply.github.com>
2018-12-02 22:35:24 +0100

    Update and rename obusAtom.c to obus.c

---
+++ obus.c
@@ -0,0 +1,79 @@
+#include "librairies.h"
+
+#include "fonctions.h"
+
+listeObus * INITIALIZE_LIST_OBUS () {
+	listeObus *liste = malloc(sizeof(listeObus));
+   	obus *unObus = malloc(sizeof(obus));
+	if (liste != NULL && unObus != NULL)
+   	{
+		unObus->Next = NULL;
+		liste->Premier = unObus;
+    	}
+
+
+	return liste;
+}
+
+void INSERT_OBUS(listeObus *liste, obus myObus)
+{
+    if (liste != NULL)
+    {
+   	 myObus.Next = liste->Premier;
+    	liste->Premier = &myObus;
+    }
+}
+
+
+void DELETE_OBUS(listeObus *liste)
+{
+    if (liste != NULL) {
+	if (liste->Premier != NULL)
+   	 {
+        obus *aSupprimer = liste->Premier;
+        liste->Premier = liste->Premier->Next;
+        free(aSupprimer);
+   	 }
+    }
+}
+
+void INITIALIZE_OBUS(obus OBUS, int estActif, int PX,int PY, int DIR) {
+	OBUS.Actif = estActif;
+	OBUS.PosX = PX;
+	OBUS.PosY = PY;
+	OBUS.Direction = DIR;
+
+
+}
+
+
+void DEPLACE_OBUS(obus OBUS, char ** map) {	
+	char c = 'O';
+	printf("\033[%d;%dH \n",OBUS.PosX,OBUS.PosY);
+	switch (OBUS.Direction) {
+		case 8 :
+			if (map[OBUS.PosX-1][OBUS.PosY]==' ') {
+				(OBUS.PosX)--;
+				printf("\033[%d;%dH%c\n",OBUS.PosX,OBUS.PosY,c);
+			} else OBUS.Actif = 0;
+			break;
+		case 2 :
+			if (map[OBUS.PosX+1][OBUS.PosY]==' ') {
+				(OBUS.PosX)++;
+				printf("\033[%d;%dH%c\n",OBUS.PosX,OBUS.PosY,c);
+			} else OBUS.Actif = 0;
+			break;
+		case 6 :
+			if (map[OBUS.PosX][OBUS.PosY+1]==' ') {
+				(OBUS.PosY)++;
+				printf("\033[%d;%dH%c\n",OBUS.PosX,OBUS.PosY,c);
+			} else OBUS.Actif = 0;
+			break;
+		case 4 :
+			if (map[OBUS.PosX][OBUS.PosY-1]==' ') {
+				(OBUS.PosY)--;
+				printf("\033[%d;%dH%c\n",OBUS.PosX,OBUS.PosY,c);
+			} else OBUS.Actif = 0;
+			break;
+	}
+}

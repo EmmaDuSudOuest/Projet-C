b021e7e150facd70557933d470232a10ce6b9794
AmySian <44503383+AmySian@users.noreply.github.com>
2018-12-02 22:09:15 +0100

    Update menuAtom.c

--- menuAtom.c
+++ menuAtom.c
@@ -1,107 +1,70 @@
 #include "librairiesAtom.h"
-
 #include "fonctionsAtom.h"
 
-
-
-void MODE_FACILE (char * nameMapFile, int mapX, int mapY, char * nameTankFile,int PX,int PY,int TX,int TY, int DIR) {			
-	char ** map;
-	int endGame;
-	map = CHARGEMENT_MAP(nameMapFile, mapY, mapX);
-	system("clear");
-
-	AFFICHAGE_MAT (108, 673,map);
-	tank theTank;
-
-	INITIALIZE_TANK(map,theTank,PX,PY,TX,TY,DIR,nameTankFile);
-	obus * listeObus;
-	listeObus = INITIALIZE_LIST_OBUS ();
-	endGame = 0;
-	clock_t start_t, t;
-	start_t = clock();
-	while (endGame == 0) {
+int MENU(int NB_L, int NB_C, char * facile, char * difficile, char * quitter) {
+	int etat = 0;
+	int sortie = 0;	
+	AFFICHAGE_MENU(NB_L*2, NB_C, facile);
+	while (sortie==0) {
+usleep(1);
+		//if (key_pressed()== '\033') {
+		//key_pressed();
 		switch (key_pressed()) {
-
-			case 'q' :
-				AFFICHAGE_MENU(214, 672, "out.txt");
-				endGame = 1;
-				break;
-
-			case 'A' :
-				theTank.Direction = 8;
-				if(map[PX-2][PY]==' '){
-					CLEAR_MYTANK (TY, TX,PX,PY);
-					PX --;
-					AFFICHE_MYTANK(nameTankFile, TY, (TX-1),(PX),PY);
-				}
-				break;
-				
+usleep(3);
 			case 'B' :
-				theTank.Direction = 2;
-				if(map[PX+8][PY]==' '){
-					CLEAR_MYTANK (TY, TX,PX,PY);
-					PX++;
-					AFFICHE_MYTANK(nameTankFile, TY, (TX-1),(PX),PY);
-				}
-		
-				break;
-
-			case 'D' :
-				theTank.Direction = 4;
-				if(map[PX][PY-3]==' '){
-					CLEAR_MYTANK (TY, TX,PX,PY);
-					PY= PY- 2;
-					AFFICHE_MYTANK(nameTankFile, TY, (TX-1),PX,PY);
+				if (etat==0) {
+					etat=1;
+					AFFICHAGE_MENU(NB_L*2, NB_C, difficile);
+				} else if (etat==1) {
+					etat=2;
+					AFFICHAGE_MENU(NB_L*2, NB_C, quitter);
 				}
 				break;
-
-			case 'C' :
-				theTank.Direction = 6;
-				if(map[PX][PY+6]==' '){
-					CLEAR_MYTANK (TY, TX,PX,PY);
-					PY= PY+2;
-					AFFICHE_MYTANK(nameTankFile, TY, (TX-1),PX,PY);
+			case 'A' :
+				if (etat==1) {
+					etat=0;
+					AFFICHAGE_MENU(NB_L*2, NB_C, facile);
+				} else if (etat==2) {
+					etat=1;
+					AFFICHAGE_MENU(NB_L*2, NB_C, difficile);
 				}
 				break;
-
-			case 'e' :
-				;
-				int i=0;
-				while (listeObus[i].Actif==1) {
-					i++;
-				}
-				listeObus[i].Actif=1;
-				listeObus[i].Direction=theTank.Direction;
-				switch (theTank.Direction) {
-
-					case 8 :
-						listeObus[i].PosX=PX;
-						listeObus[i].PosY=PY + TX/2;
-						break;
-					case 2 :
-						listeObus[i].PosX=PX + TY;
-						listeObus[i].PosY=PY + TX/2;
-						break;
-					case 6 :
-						listeObus[i].PosX=PX + TY/2;
-						listeObus[i].PosY=PY + TX;
-						break;
-					case 4 :
-						listeObus[i].PosX=PX + TY/2;
-						listeObus[i].PosY=PY;
-						break;
-				}
-					
-				
-		}
-		//t = clock();
-		for (int i=0;i<50;i++) {
-			if (listeObus[i].Actif==1 /*&& t%5000 == 0*/) {
-				DEPLACE_OBUS(listeObus[i], map);
-				printf("numObus: %d",i);
-			}
-		}
-	
+			case '\n' :
+				sortie =1;
+				return etat;				
+		} /*else if (key_pressed()== 'q') {
+				sortie =1;
+				return etat;
+			}*/
 	}
 }
+
+void AFFICHAGE_MENU (int NB_L, int NB_C, char * nom_fic) {
+	char ligne[NB_C];
+	char c;
+	FILE * fic = fopen(nom_fic,"r");
+	system("clear");
+	for (int i=0;i<NB_L;i++) {
+             fgets(ligne, NB_C, fic);
+	     int c = printf("%s", ligne);
+	    
+	       /*switch(c){
+			case '6': printf("\e[49m|");
+                                  break;
+                        case '5': printf("\e[31mR \e[0m");
+                                  break;
+                        case '4': printf("\e[31mT\e[0m");
+                                  break;
+			case '3': printf("\e[43mI \e[0m");
+                                  break;
+                        case '2': printf("\e[31mP \e[0m");
+                                  break;
+                        case '1': printf("\e[31mM \e[0m");
+                                  break;
+	       }*/
+	       }
+				//ligne = "";
+}
+
+
 
